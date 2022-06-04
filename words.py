827791d7d5fb45699bdfeea63aef1694b37663d0
BeratDM <44789586+BeratDM@users.noreply.github.com>
2022-06-04 16:46:17 +0000

    Added some more words

--- words.py
+++ words.py
@@ -1,8 +1,8 @@
 weather_words = ["hava durumu", "weather", "havalar", "bugün hava", "hava", "havada"]
 
-weather_responses1 = ["Today is an overestimated ", "Today will be an exotic ", "Today is going to be a surprising ", "Today is an underestimated ", "Nothing surprising, Today will be "]
+weather_responses1 = ["", "Today is an overestimated ", "Today will be an exotic ", "Today is going to be a surprising ", "Today is an underestimated ", "Nothing surprising, Today will be ", "Currently it is "]
 
-weather_responses2 = ["Keeping you alive will continue to be my top priority.", "Consider getting into suitable bodywear that will extend your life expectancy.", "Don't forget to take the motion tracker with you.", "I suspect the outside may be more dangerous than you think.", "Remember, they mostly come out at night.. mostly."]
+weather_responses2 = ["", "Keeping you alive will continue to be my top priority.", "Consider getting into suitable bodywear that will extend your life expectancy.", "Don't forget to take the motion tracker with you.", "I suspect the outside may be more dangerous than you think.", "Remember, they mostly come out at night.. mostly."]
 
 weather_responses3 = ["Have a nice day.", "Stay happy.", "Good luck.", "Session Over.", "Terminating Session..", "Ending.."]
 
